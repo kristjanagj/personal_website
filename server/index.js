b7d063867f8a0aed5e63e04f1253c6f41818e9ba
Kristi <kristi.gjoka92@gmail.com>
2024-09-15 11:59:33 +0200

    My profile

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-s
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
